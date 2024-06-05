32f09d72a0b669f482f9464abddb32c71c88e413
Margaret <rixten.6@gmail.com>
2024-06-05 09:00:53 +0300

    changing data type storage

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -3,10 +3,11 @@ import { ValueFields } from '../value-fields/ValueFields';
 import { InfoBoard } from '../info-board/InfoBoard';
 import { ControlButtons } from '../control-buttons/ControlButtons';
 import { styled } from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { max, start, board, set, reset, inc } from '../../mock/data';
 import { TBoard, TValue } from '../../types/types';
 import { SetValue } from '../set-value/SetValue';
+import { Field } from '../field/Field';
 
 export const Counter = () => {
 	const [isShownSettings, setIsShownSettings] = useState(false);
@@ -24,109 +25,29 @@ export const Counter = () => {
 			disabled: false,
 		},
 	]);
-	const [boardValue, setBoardValue] = useState<TBoard>({
-		id: board,
-		value: 'enter values and press "set"',
-		error: false,
+
+	const [values, setValues] = useState({
+		startValue: '0',
+		maxValue: '0',
+		boardValue: 'enter values and press "set"',
 	});
-	const [values, setValues] = useState<TValue[]>([
-		{
-			id: start,
-			value: '0',
-			title: 'start value',
-			error: false,
-		},
-		{
-			id: max,
-			value: '0',
-			title: 'max value',
-			error: false,
-		},
-	]);
-
-	const onChangeValue = (id: string, value: string) => {
-		const currentValue = values.find(item => item.id === id);
-		if (+value < 0 || !currentValue) {
-			setValues(values.map(item => (item.id === id ? { ...item, value, error: true } : item)));
-		} else {
-			if (currentValue.id === start) {
-				switch (true) {
-					case +value === +values[1].value:
-						return setValues(
-							values.map(item => (item.id === id ? { ...item, value, error: true } : { ...item, error: true }))
-						);
-
-					case +value > +values[1].value:
-						return setValues(
-							values.map(item => (item.id === id ? { ...item, value, error: true } : { ...item, error: true }))
-						);
-
-					default:
-						return setValues(
-							values.map(item =>
-								item.id === id
-									? { ...item, value, error: false }
-									: { ...item, error: +values[1].value < 0 ? true : false }
-							)
-						);
-				}
-			}
-			if (currentValue.id === max) {
-				switch (true) {
-					case +value === +values[0].value:
-						return setValues(
-							values.map(item => (item.id === id ? { ...item, value, error: true } : { ...item, error: true }))
-						);
-
-					case +value < +values[0].value:
-						return setValues(
-							values.map(item => (item.id === id ? { ...item, value, error: true } : { ...item, error: true }))
-						);
-
-					default:
-						return setValues(
-							values.map(item =>
-								item.id === id
-									? { ...item, value, error: false }
-									: { ...item, error: +values[0].value < 0 ? true : false }
-							)
-						);
-				}
-			}
-		}
-	};
+	const startValueError = +values.startValue >= +values.maxValue || +values.startValue < 0;
+	const maxValueError = +values.startValue >= +values.maxValue || +values.maxValue < 0;
+	const boardValueError = +values.boardValue === +values.startValue;
 
 	const onToggle = () => {
 		setIsShownSettings(true);
 	};
 
-	const onChangeIncHandler = () => {
-		if (+boardValue.value + 1 === +values[1].value) {
-			setBoardValue({ ...boardValue, value: (+boardValue.value + 1).toString(), error: true });
-		}
-		if (+boardValue.value + 1 < +values[1].value) {
-			setBoardValue({ ...boardValue, value: (+boardValue.value + 1).toString() });
-		}
-	};
+	const onChangeIncHandler = () => {};
 
-	const onChangeResetHandler = () => {
-		setBoardValue({ ...boardValue, value: values[0].value, error: false });
-	};
+	const onChangeResetHandler = () => {};
 
-	const setValuesHandler = () => {
-		localStorage.setItem('values', JSON.stringify(values));
-		localStorage.setItem('board', JSON.stringify({ ...boardValue, value: values[0].value }));
-		setBoardValue({ ...boardValue, value: values[0].value });
-		setIsShownSettings(false);
-	};
+	const setValuesHandler = () => {};
 
 	useEffect(() => {
 		const storageValues = localStorage.getItem('values');
 		const storageBoard = localStorage.getItem('board');
-		if (storageValues && storageBoard) {
-			setValues(JSON.parse(storageValues));
-			setBoardValue(JSON.parse(storageBoard));
-		}
 	}, []);
 
 	return (
@@ -134,7 +55,9 @@ export const Counter = () => {
 			<FlexWrapper gap='20px' direction='column'>
 				{isShownSettings ? (
 					<>
-						<ValueFields values={values} onChangeValue={onChangeValue} />
+						{/* <ValueFields values={values} onChangeValue={onChangeValue} /> */}
+						<Field value={item.value} callback={onChange} error={startValueError} label={'start value'} />
+						<Field value={item.value} callback={onChange} error={item.error} label={'max value'} />
 						<SetValue values={values} setValuesHandler={setValuesHandler} />
 					</>
 				) : (
